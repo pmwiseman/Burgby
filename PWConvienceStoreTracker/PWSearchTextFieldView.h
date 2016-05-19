3019292f1fc446f979d941159170179537e9d125
Patrick <patrickmwiseman@gmail.com>
2016-05-18 23:14:27 -0400

    updated for location search and more flexible layouts

---
+++ PWConvienceStoreTracker/PWSearchTextFieldView.h
@@ -0,0 +1,13 @@
+//
+//  PWSearchTextFieldView.h
+//  PWConvienceStoreTracker
+//
+//  Created by Patrick Wiseman on 5/18/16.
+//  Copyright © 2016 Patrick Wiseman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PWSearchTextFieldView : UIView
+
+@end
---
+++ PWConvienceStoreTracker/PWSearchTextFieldView.m
@@ -0,0 +1,21 @@
+//
+//  PWSearchTextFieldView.m
+//  PWConvienceStoreTracker
+//
+//  Created by Patrick Wiseman on 5/18/16.
+//  Copyright © 2016 Patrick Wiseman. All rights reserved.
+//
+
+#import "PWSearchTextFieldView.h"
+
+@implementation PWSearchTextFieldView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
